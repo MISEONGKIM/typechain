20ac8bf32c9e25345de291c14ef1508424e2ae76
MISEONGKIM <MISEONGKIM@github.com>
2021-01-14 16:16:00 +0900

    interface예제

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-const sayHello = (name:string):string => {
-    return `HELLO ${name}!!!`
+interface Human {
+    name:string
+}
+const person = {
+    name: 'sss'
+}
+const sayHello = (person:Human):string => {
+    return `HELLO ${person.name}!!!`
 }
 
-console.log(sayHello("ms"));
+console.log(sayHello(person));
 
 export {};
